097076e3165a9a9cf4927905685cde5d67bfe8b9
nasojako <nasojako@aol.com>
2016-12-07 15:44:37 -0600

    Update ultrasonic.ino

--- ultrasonic/ultrasonic.ino
+++ ultrasonic/ultrasonic.ino
@@ -1,51 +1,8 @@
 //// ---------------------------------------------------------------------------
-//// Example NewPing library sketch that does a ping about 20 times per second.
+//// Jason Robles
+//// CSCE_462 502
 //// ---------------------------------------------------------------------------
 //
-//#include <NewPing.h>
-//
-//#define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
-//#define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
-//#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
-//
-//NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
-//
-//void setup() {
-//  Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
-//}
-//
-//void loop() {
-//  delay(50);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
-//  Serial.print("Ping: ");
-//  Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
-//  Serial.println("cm");
-//}
-
-
-// Ping Example
-/* Ping))) Sensor
-
-   This sketch reads a PING))) ultrasonic rangefinder and returns the
-   distance to the closest object in range. To do this, it sends a pulse
-   to the sensor to initiate a reading, then listens for a pulse
-   to return.  The length of the returning pulse is proportional to
-   the distance of the object from the sensor.
-
-   The circuit:
-  * +V connection of the PING))) attached to +5V
-  * GND connection of the PING))) attached to ground
-  * SIG connection of the PING))) attached to digital pin 7
-
-   http://www.arduino.cc/en/Tutorial/Ping
-
-   created 3 Nov 2008
-   by David A. Mellis
-   modified 30 Aug 2011
-   by Tom Igoe
-
-   This example code is in the public domain.
-
- */
 
 // this constant won't change.  It's the pin number
 // of the sensor's output:
@@ -62,8 +19,6 @@ void setup() {
 }
 
 void loop() {
-  // establish variables for duration of the ping,
-  // and the distance result in inches and centimeters:
   long duration, inches, cm;
 
   // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
@@ -108,17 +63,9 @@ void loop() {
 }
 
 long microsecondsToInches(long microseconds) {
-  // According to Parallax's datasheet for the PING))), there are
-  // 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
-  // second).  This gives the distance travelled by the ping, outbound
-  // and return, so we divide by 2 to get the distance of the obstacle.
-  // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
   return microseconds / 74 / 2;
 }
 
 long microsecondsToCentimeters(long microseconds) {
-  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
-  // The ping travels out and back, so to find the distance of the
-  // object we take half of the distance travelled.
   return microseconds / 29 / 2;
 }
