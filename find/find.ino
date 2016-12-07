ffbde7413b90cbb58c7e2fa71316c376719e85e0
nasojako <nasojako@aol.com>
2016-12-07 15:45:15 -0600

    Update find.ino

--- find/find.ino
+++ find/find.ino
@@ -1,6 +1,7 @@
 /*
  * App to locate stick by sounding alarm over Bluetooth
  * Jason Robles
+ * CSCE-462
  */
 
 #include <CurieBLE.h>
@@ -128,18 +129,10 @@ void loop() {
 
 // functions to convert distances
 long microsecondsToInches(long microseconds) {
-  // According to Parallax's datasheet for the PING))), there are
-  // 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
-  // second).  This gives the distance travelled by the ping, outbound
-  // and return, so we divide by 2 to get the distance of the obstacle.
-  // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
   return microseconds / 74 / 2;
 }
 
 long microsecondsToCentimeters(long microseconds) {
-  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
-  // The ping travels out and back, so to find the distance of the
-  // object we take half of the distance travelled.
   return microseconds / 29 / 2;
 }
 
